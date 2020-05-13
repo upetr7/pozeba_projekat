a709fa4c64ec1ff8bada9ee60e8715290e3c96cb
uros petrovic <upetr0227@gmail.com>
2020-05-13 14:15:53 +0200

    php kod za azur xml

--- azur-xml.php
+++ azur-xml.php
@@ -13,11 +13,55 @@
     <?php
         include('inc/header.inc.html');
 
-        // if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        //     echo '<script>console.log(\'post method\')</script>';
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $dom = new DOMDocument();
+            $url_sadnice = 'xml/sadnice.xml';
+            $url_pozebe = 'xml/pozebe.xml';
 
-        //     include('inc/azur-uspesno.inc.html');
-        // }
+            $post_key = array_keys($_POST);
+            $arr_key_sadnice = ['name', 'location', 'temperature', 'desc'];
+            $arr_key_pozebe = ['key1', 'key2', 'key3']; 
+
+            // Odredjivanje koju xml datoteku treba popuniti
+            $type; // type=0 sadnice | type=false pozebe 
+            $type = strpos($post_key[1], $arr_key_sadnice[1]);
+
+            
+            
+            if($type !== false) {
+                $dom->load($url_sadnice);
+                $sadnice = $dom->firstChild;
+                // trebace count()/count() - 1 zbog poslednjeg inputa koji je prazan
+                for($j = 0; $j < (count($_POST) / count($arr_key_sadnice)) - 1; $j++){
+                    $el = $dom->createElement('sadnica');
+                    for($i = 0; $i < count($arr_key_sadnice); $i++) {
+                        $field = $dom->createElement($arr_key_sadnice[$i], $_POST[$post_key[$i + $j * 3]]);
+                        $el->appendChild($field);
+                    }
+                    print_r($el);
+                    $sadnice->appendChild($el);
+                }
+                echo $dom->save($url_sadnice);
+            } else {
+                $dom->load($url_pozebe);
+                $pozebe = $dom->firstChild;
+                for($j = 0; $j < (count($_POST) / count($arr_key_pozebe)) - 1; $j++){
+                    $el = $dom->createElement('sadnica');
+                    for($i = 0; $i < count($arr_key_pozebe); $i++) {
+                        $field = $dom->createElement($arr_key_pozebe[$i], $_POST[$post_key[$i + $j * 3]]);
+                        $el->appendChild($field);
+                    }
+                    $pozebe->appendChild($el);
+                }
+                echo $dom->save($url_pozebe);
+            }
+            
+
+
+
+
+            // include('inc/azur-uspesno.inc.html');
+        }
         
     ?>
 
@@ -30,6 +74,7 @@
                 <li>Pozebe</li>
             </ul>
         </div>
+        <!-- sadnice form -->
         <form action="azur-xml.php" method="post" id="sadnice">
             <div class="fields-nm">
                 <ul>
@@ -39,17 +84,11 @@
                     <li>Itd</li>
                 </ul>
             </div>
-            <div class="fields-vl">
-                <input type="text" name="name-1">
-                <input type="text" name="location-1">
-                <input type="text" name="temperature-1">
-                <input type="text" name="etc-1">
-                <div class="delete">X</div>
-            </div>
         </form>
-        <button  id="btn-azur-sadnice" disabled="disabled">Azuriraj</button>
-        <!-- type="submit" form="sadnice" -->
-        <!-- <form action="azur-xml.php" method="post" id="pozebe">
+        <button  id="btn-azur-sadnice" disabled="disabled" form="sadnice">Azuriraj sadnice</button>
+        <!-- end sadnice form -->
+        <form action="azur-xml.php" method="post" id="pozebe">
+            pozebe
             <div class="fields-nm">
                 <ul>
                     <li>Naziv</li>
@@ -58,31 +97,12 @@
                     <li>Itd</li>
                 </ul>
             </div>
-            <div class="fields-vl">
-                <input type="text" name="naziv-1">
-                <input type="text" name="lokacija-1">
-                <input type="text" name="temperatura-1">
-                <input type="text" name="itd-1">
-            </div>
-            <div class="fields-vl">
-                <input type="text" name="naziv-1">
-                <input type="text" name="lokacija-1">
-                <input type="text" name="temperatura-1">
-                <input type="text" name="itd-1">
-            </div>
-            <div class="fields-vl">
-                <input type="text" name="naziv-1">
-                <input type="text" name="lokacija-1">
-                <input type="text" name="temperatura-1">
-                <input type="text" name="itd-1">
-            </div>
-        </form> -->
-
+        </form>
+        <button  id="btn-azur-pozebe" disabled="disabled" form="pozebe">Azuriraj pozebe</button>
+        <!-- end pozebe form -->
     </div>
     <!-- SCRIPT -->
     <script src="js/azur-xml.js"></script>
-    <script src="js/azur-sadnica-xml.js"></script>
-    <script src="js/azur-pozeba-xml.js"></script>
 </body>
 
 </html>
